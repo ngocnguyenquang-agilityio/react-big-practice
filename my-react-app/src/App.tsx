dd4e5cbf0f864b952eab8b4e6ec681266368ee7a
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-01-31 15:30:44 +0700

    feat: update contact component

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,28 +1,10 @@
 import "./App.css";
 import Contact from "./SWR/Contact";
-import useSWR from "swr";
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const App = () => {
-  const { data, error } = useSWR(
-    "https://jsonplaceholder.typicode.com/users",
-    fetcher
-  );
-
-  if (error) return <p>An error occurred</p>;
-  if (!data) return <p>Loading</p>;
-
   return (
     <div className="App">
-      {data.map(({ id, name, email, company }) => (
-        <Contact
-          key={id}
-          name={name}
-          email={email}
-          tagline={company.catchPhrase}
-        />
-      ))}
+      <Contact />
     </div>
   );
 };
--- my-react-app/src/SWR/Contact.tsx
+++ my-react-app/src/SWR/Contact.tsx
@@ -1,13 +1,33 @@
-const Contact = ({
-  name = "Steve Jobs",
-  email = "steve@apple.com",
-  tagline = "Stay hungry, stay foolish",
-}) => {
+import useSWR from "swr";
+
+const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
+
+const Contact = () => {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
+
+  if (error) return <p>An error occurred</p>;
+  if (!data) return <p>Loading</p>;
   return (
     <div className="contact">
-      <h5 className="contact__name">{name}</h5>
-      <h6 className="contact__email">{email}</h6>
-      <p className="contact__tagline">{tagline}</p>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Company</th>
+        </thead>
+        <tbody>
+          {data.map(({ id, name, email, company }) => (
+            <tr key={id}>
+              <td>{name}</td>
+              <td>{email}</td>
+              <td>{company.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
