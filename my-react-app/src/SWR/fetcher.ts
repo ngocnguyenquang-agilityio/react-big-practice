d1043bfa496e230a54422a51c14c0d2db1dff7e1
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-02-01 15:59:56 +0700

    feat: error handling

--- my-react-app/src/SWR/fetcher.ts
+++ my-react-app/src/SWR/fetcher.ts
@@ -1,1 +1,12 @@
-export const fetcher = (url: string) => fetch(url).then(res => res.json());
+export const fetcher = async (url: string) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching data");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
