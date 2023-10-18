7cb652f72297855721f3d440bed75a143affcb0b
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-10-18 09:28:47 +0700

    feat: #112 update button size storybook

--- practices/big-practice/src/components/storybook/Button.stories.tsx
+++ practices/big-practice/src/components/storybook/Button.stories.tsx
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button, ButtonProps } from '../ui/button'
-import { Menu } from 'lucide-react';
-
 
 export default {
   title: 'Components/Button',
@@ -40,7 +38,32 @@ export const Disabled: Story = {
 export const IconButton: Story = {
   render: () => (
     <Button variant='outline' size='icon'>
-      <Menu />
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-menu">
+        <line x1="4" x2="20" y1="12" y2="12" />
+        <line x1="4" x2="20" y1="6" y2="6" />
+        <line x1="4" x2="20" y1="18" y2="18" />
+      </svg>
     </Button>
   )
+}
+
+export const SmallButton: Story = {
+  args: {
+    variant: 'default',
+    size: 'sm'
+  }
+}
+
+export const LargeButton: Story = {
+  args: {
+    variant: 'default',
+    size: 'lg'
+  }
+}
+
+export const FullButton: Story = {
+  args: {
+    variant: 'default',
+    size: 'xl'
+  }
 }
