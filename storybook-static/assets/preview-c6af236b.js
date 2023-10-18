29e6dbbb6a50172c15ead890577bd9b7b1188afd
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-10-18 10:24:11 +0700

    install vercel

---
+++ storybook-static/assets/preview-c6af236b.js
@@ -0,0 +1,2 @@
+const a={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},backgrounds:{default:"dark",value:{name:"dark",value:"#000"}}}};export{a as default};
+//# sourceMappingURL=preview-c6af236b.js.map
