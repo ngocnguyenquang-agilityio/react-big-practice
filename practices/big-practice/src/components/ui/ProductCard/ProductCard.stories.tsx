68d7e33908d0c5fd383017dea3ce1d91a4190d1a
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-10-20 09:21:18 +0700

    feat: #135 update product card

--- practices/big-practice/src/components/ui/ProductCard/ProductCard.stories.tsx
+++ practices/big-practice/src/components/ui/ProductCard/ProductCard.stories.tsx
@@ -11,9 +11,9 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    productName: 'Test Product',
+    name: 'Test Product',
     price: 20,
-    imageLink: 'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75'
+    image: 'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75'
   }
 }
 
--- practices/big-practice/src/components/ui/ProductCard/ProductCard.tsx
+++ practices/big-practice/src/components/ui/ProductCard/ProductCard.tsx
@@ -1,23 +1,23 @@
 export interface IProductCard {
-  productName: string,
+  name: string,
   price: number,
-  imageLink: string
+  image: string
 }
 
-const ProductCard = ({ productName, price, imageLink }: IProductCard): JSX.Element => {
+const ProductCard = ({ name, price, image }: IProductCard): JSX.Element => {
   return (
     <div className="aspect-square transition-opacity animate-fadeIn">
-      {/* TODO: Update height and width to full later */}
-      <div className="group flex h-1/3 w-1/3 items-center justify-center overflow-hidden rounded-lg border hover:border-blue-600 bg-black relative border-neutral-800">
+      <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border hover:border-blue-600 bg-black relative border-neutral-800">
         <img
+          // TODO: Add lazy load for image
           className="relative h-full w-full object-contain transition duration-300 ease-in-out group-hover:scale-105"
           loading="lazy"
-          alt={productName}
-          src={imageLink}
+          alt={name}
+          src={image}
         />
         <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4">
           <div className="flex items-center rounded-full border p-1 text-xs font-semibold backdrop-blur-md border-neutral-800 bg-black/70 text-white">
-            <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">{productName}</h3>
+            <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">{name}</h3>
             <p className="flex-none rounded-full bg-blue-600 p-2 text-white">
               {price}
               <span className="ml-1 inline @[275px]/label:inline">USD</span>
