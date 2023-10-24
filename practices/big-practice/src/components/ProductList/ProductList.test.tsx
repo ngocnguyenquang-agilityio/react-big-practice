1fe3036eed08922c53ee478707539babb705c1e1
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-10-24 21:45:07 +0700

    feat: #125 get list products

--- practices/big-practice/src/components/ProductList/ProductList.test.tsx
+++ practices/big-practice/src/components/ProductList/ProductList.test.tsx
@@ -5,34 +5,44 @@ import ProductList from '.';
 
 const mockProducts = [
   {
-    name: 'Product 1',
+    id: 1,
+    title: 'Product 1',
     price: 20,
-    image:
+    category: 'phone',
+    images: [
       'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
-    size: ['XS', 'S', 'M', 'L'],
-    color: ['black', 'white', 'blue'],
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
+    ],
+    thumbnail:
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
   },
   {
-    name: 'Product 2',
-    price: 10,
-    image:
+    id: 2,
+    title: 'Product 2',
+    price: 20,
+    category: 'phone',
+    images: [
       'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
-    size: ['XS', 'S', 'M', 'L'],
-    color: ['black', 'white', 'blue'],
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
+    ],
+    thumbnail:
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=3840&q=75',
   },
 ];
 
 describe('ProductList Component', () => {
-    let comp: RenderResult;
-    beforeEach(() => {
-      comp = render(<ProductList products={mockProducts} />);
-    });
-  
-    test('Should render successfully', () => {
-      expect(comp).toBeTruthy();
-    });
-  
-    test('Should match snapshot', () => {
-      expect(comp).toMatchSnapshot();
-    });
-})
+  let comp: RenderResult;
+  beforeEach(() => {
+    comp = render(<ProductList products={mockProducts} />);
+  });
+
+  test('Should render successfully', () => {
+    expect(comp).toBeTruthy();
+  });
+
+  test('Should match snapshot', () => {
+    expect(comp).toMatchSnapshot();
+  });
+});
--- practices/big-practice/src/pages/HomePage/index.tsx
+++ practices/big-practice/src/pages/HomePage/index.tsx
@@ -1,27 +1,20 @@
+// Libs
+import useSWR from 'swr';
+
 // Components
 import Collection from '@components/Collection';
 import ProductList from '@components/ProductList';
 import HomeLayout from '@layouts/HomeLayout';
+import { fetcher } from '@services/fetcher';
+import Loading from '@components/Loading';
 
 const mockListCollection = ['All', 'Hoodie', 'Jacket', 'Shirt'];
-// TODO: Will remove after fetching
-const mockProductList = [
-  {
-    name: 'Test Product 1',
-    price: 20,
-    image:
-      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75',
-  },
-
-  {
-    name: 'Test Product 2',
-    price: 10,
-    image:
-      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75',
-  },
-];
 
 const HomePage = () => {
+  const { data, isLoading } = useSWR('https://dummyjson.com/products?limit=20&select=title,price,images,category,thumbnail,id', fetcher);
+
+  if(isLoading) return <Loading />
+
   return (
     <HomeLayout
       leftAside={
@@ -32,12 +25,12 @@ const HomePage = () => {
       }
       rightAside={
         <Collection
-          list={['Price: Low to Hight', 'Price: High to Low']}
+          list={['Price: Low to High', 'Price: High to Low']}
           title='Sort by'
         />
       }
     >
-      <ProductList products={mockProductList} />
+      <ProductList products={data.products} />
     </HomeLayout>
   );
 };
