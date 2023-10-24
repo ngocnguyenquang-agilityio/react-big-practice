37d4ddd9f33ec4473138c5987f5b01833eb82e6e
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-10-24 17:34:39 +0700

    feat: #138 create product list component

--- practices/big-practice/src/pages/HomePage/index.tsx
+++ practices/big-practice/src/pages/HomePage/index.tsx
@@ -1,9 +1,25 @@
 // Components
 import Collection from '@components/Collection';
-import ProductCard from '@components/ProductCard';
+import ProductList from '@components/ProductList';
 import HomeLayout from '@layouts/HomeLayout';
 
 const mockListCollection = ['All', 'Hoodie', 'Jacket', 'Shirt'];
+// TODO: Will remove after fetching
+const mockProductList = [
+  {
+    name: 'Test Product 1',
+    price: 20,
+    image:
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75',
+  },
+
+  {
+    name: 'Test Product 2',
+    price: 10,
+    image:
+      'https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75',
+  },
+];
 
 const HomePage = () => {
   return (
@@ -21,29 +37,7 @@ const HomePage = () => {
         />
       }
     >
-      <ul className='grid grid-flow-row gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
-        <li className='aspect-square transition-opacity animate-fadeIn'>
-          <ProductCard
-            name='Test Product'
-            price={20}
-            image='https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75'
-          />
-        </li>
-        <li className='aspect-square transition-opacity animate-fadeIn'>
-          <ProductCard
-            name='Test Product'
-            price={20}
-            image='https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75'
-          />
-        </li>
-        <li className='aspect-square transition-opacity animate-fadeIn'>
-          <ProductCard
-            name='Test Product'
-            price={20}
-            image='https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75'
-          />
-        </li>
-      </ul>
+      <ProductList products={mockProductList} />
     </HomeLayout>
   );
 };
