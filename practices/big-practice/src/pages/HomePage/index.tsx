8fd27fc8b5076985f9be7f92ea6c509bc0cd7dc6
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-10-24 17:21:30 +0700

    feat: #138 update page layout

--- practices/big-practice/src/pages/HomePage/index.tsx
+++ practices/big-practice/src/pages/HomePage/index.tsx
@@ -1,26 +1,42 @@
 // Components
-import ProductCard from "@components/ProductCard";
-import HomeLayout from "@layouts/HomeLayout";
+import Collection from '@components/Collection';
+import ProductCard from '@components/ProductCard';
+import HomeLayout from '@layouts/HomeLayout';
+
+const mockListCollection = ['All', 'Hoodie', 'Jacket', 'Shirt'];
 
 const HomePage = () => {
   return (
-    <HomeLayout>
-      <ul className="grid grid-flow-row gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        <li className="aspect-square transition-opacity animate-fadeIn">
+    <HomeLayout
+      leftAside={
+        <Collection
+          list={mockListCollection}
+          title='Collection'
+        />
+      }
+      rightAside={
+        <Collection
+          list={['Price: Low to Hight', 'Price: High to Low']}
+          title='Sort by'
+        />
+      }
+    >
+      <ul className='grid grid-flow-row gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+        <li className='aspect-square transition-opacity animate-fadeIn'>
           <ProductCard
             name='Test Product'
             price={20}
             image='https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75'
           />
         </li>
-        <li className="aspect-square transition-opacity animate-fadeIn">
+        <li className='aspect-square transition-opacity animate-fadeIn'>
           <ProductCard
             name='Test Product'
             price={20}
             image='https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fkeyboard.png%3Fv%3D1690003507&w=3840&q=75'
           />
         </li>
-        <li className="aspect-square transition-opacity animate-fadeIn">
+        <li className='aspect-square transition-opacity animate-fadeIn'>
           <ProductCard
             name='Test Product'
             price={20}
@@ -29,7 +45,7 @@ const HomePage = () => {
         </li>
       </ul>
     </HomeLayout>
-  )
+  );
 };
 
 export default HomePage;
