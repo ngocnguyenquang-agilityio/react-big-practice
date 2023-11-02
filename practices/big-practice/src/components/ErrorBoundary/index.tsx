7f531d2a06430884aef06fd533373ccf077fa474
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-11-02 10:17:07 +0700

    feat: update error boundary

--- practices/big-practice/src/components/ErrorBoundary/index.tsx
+++ practices/big-practice/src/components/ErrorBoundary/index.tsx
@@ -1,15 +1,21 @@
 // Components
-import { Button } from "@components/Button";
+import { Button } from '@components/Button';
+import { useRouteError } from 'react-router-dom';
 
 export const ErrorBoundary = () => {
+  const error = useRouteError();
+  const onClickReloadPage = () => {
+    window.location.reload();
+  };
+
   return (
     <div className='error-page min-h-screen flex items-center justify-center'>
       <div className='text-center'>
         <h1 className='text-5xl font-semibold text-red-600 mb-4'>Something went wrong</h1>
-        <p className='text-2xl text-gray-300 font-semibold'>Unknown Error</p>
+        <p className='text-2xl text-gray-300 font-semibold'>{error.message}</p>
         <Button
           className='mt-4'
-          onClick={() => window.location.reload()}
+          onClick={onClickReloadPage}
         >
           Reload Page
         </Button>
