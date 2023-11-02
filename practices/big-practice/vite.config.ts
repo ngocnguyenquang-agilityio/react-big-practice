9082591036e334312e4406fea391880cd276ad34
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-11-02 10:26:40 +0700

    feat: remove comment

--- practices/big-practice/vite.config.ts
+++ practices/big-practice/vite.config.ts
@@ -39,19 +39,4 @@ export default defineConfig({
       },
     }),
   ],
-  // resolve: {
-  //   alias: {
-  //     '@assets': `${path.resolve(__dirname, './src/assets/')}`,
-  //     '@components': `${path.resolve(__dirname, './src/components/')}`,
-  //     '@constants': `${path.resolve(__dirname, './src/constants/')}`,
-  //     '@lib': `${path.resolve(__dirname, './src/lib/')}`,
-  //     '@layouts': `${path.resolve(__dirname, './src/layouts/')}`,
-  //     '@pages': `${path.resolve(__dirname, './src/pages/')}`,
-  //     '@interfaces': `${path.resolve(__dirname, './src/interfaces/')}`,
-  //     '@services': `${path.resolve(__dirname, './src/services/')}`,
-  //     '@hooks': `${path.resolve(__dirname, './src/hooks/')}`,
-  //     '@stores': `${path.resolve(__dirname, './src/stores/')}`,
-  //     '@helpers': `${path.resolve(__dirname, './src/helpers/')}`,
-  //   },
-  // },
 });
