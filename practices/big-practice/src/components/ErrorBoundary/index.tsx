b3b4f9c842f8bef995b32d3f4eb9a86436ab7ba5
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-11-02 10:20:07 +0700

    feat: update error boundary

--- practices/big-practice/src/components/ErrorBoundary/index.tsx
+++ practices/big-practice/src/components/ErrorBoundary/index.tsx
@@ -12,7 +12,7 @@ export const ErrorBoundary = () => {
     <div className='error-page min-h-screen flex items-center justify-center'>
       <div className='text-center'>
         <h1 className='text-5xl font-semibold text-red-600 mb-4'>Something went wrong</h1>
-        <p className='text-2xl text-gray-300 font-semibold'>{error.message}</p>
+        <details className='text-2xl text-gray-300 font-semibold'>{error.message}</details>
         <Button
           className='mt-4'
           onClick={onClickReloadPage}
