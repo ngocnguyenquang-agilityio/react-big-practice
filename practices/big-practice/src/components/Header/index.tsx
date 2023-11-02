b7f0d17abbe61192716383e0f9af111ee231025f
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-11-02 22:07:56 +0700

    feat: #128 update show number of item in cart

--- practices/big-practice/src/components/Header/index.tsx
+++ practices/big-practice/src/components/Header/index.tsx
@@ -92,11 +92,11 @@ const Header = memo(({ onToggleCart }: { onToggleCart: () => void }) => {
                 svg={cartIcon}
                 name='cart-icon'
               />
-              {cartItems.length !== 0 ? (
+              {cartItems.length !== 0 && (
                 <div className='absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded bg-blue-600 text-[11px] font-small text-white'>
                   {cartItems.reduce((total, currentItem) => (total += currentItem.quantity), 0)}
                 </div>
-              ) : null}
+              )}
             </div>
           </Button>
         </div>
